dec7865876bf028effe6fb79cc772c7d91baa32d
OtavioAndretta <otavio12257@gmail.com>
2026-02-09 09:27:23 -0300

    Criando conversas por inbox diferentes --- odoo16 funcional

--- chatwoot_odoo_activity_notify/models/mail_activity.py
+++ chatwoot_odoo_activity_notify/models/mail_activity.py
@@ -11,28 +11,64 @@ class MailActivity(models.Model):
         domain=[('account_id', '=', "1")],
     )
 
+    
+
     def action_notify_activity_cron(self):
         MailActivity = self.env['mail.activity']
-        self.chatwoot_id = chatwoot_id = self.env['chatwoot.instance'].search([("account_id", "=", "1")], limit=1)
-        vencimento = fields.Date.today() + timedelta(days=chatwoot_id.activity_due_days)
-        activities = MailActivity.search([('date_deadline', '=', vencimento)])
-        status = "resolved"
-        team_id = 36 #ATS INTERNO
+
+        chatwoot = self.env['chatwoot.instance'].search(
+            [("account_id", "=", "1")],
+            limit=1
+        )
+
+        if not chatwoot:
+            return
+
+        inbox = self.env['chatwoot.inbox'].search(
+            [('instance_id', '=', chatwoot.id)],
+            limit=1
+        )
+
+        if not inbox:
+            return
+
+        vencimento = fields.Date.today() + timedelta(days=chatwoot.activity_due_days)
+
+        activities = MailActivity.search([
+            ('date_deadline', '=', vencimento)
+        ])
+
+        team_id = 36        # ATS
+        assignee_id = 3     # usuário chatwoot
+
         for activity in activities:
+
             notify = f"""
-            Olá {activity.user_id.name},
-            Você tem uma atividade pendente:
-                Tipo: {activity.activity_type_id.name}
-                Assunto: {activity.summary}
-                Nota: {activity.note}
-            Vencimento: {activity.date_deadline}
-            """
-            phone_number = f"{activity.user_id.partner_id.phone_sanitized}"
-            conversation_id = chatwoot_id.create_new_conversation(phone_number, activity.user_id.partner_id, status, team_id, notify)
+    Olá {activity.user_id.name},
+    Você tem uma atividade pendente:
+
+    Tipo: {activity.activity_type_id.name}
+    Assunto: {activity.summary}
+    Nota: {activity.note}
+    Vencimento: {activity.date_deadline}
+    """
+
+            phone_number = activity.user_id.partner_id.phone_sanitized
+
+            conversation = chatwoot.create_new_conversation(
+                phone_number,
+                activity.user_id.partner_id,
+                team_id,
+                assignee_id,
+                inbox
+            )
+
+            conversation_id = conversation.get("id")
+
             if conversation_id:
-                conversation_id = conversation_id.get('id')
-            # chatwoot_id.send_text(conversation_id, notify)
-            # chatwoot_id.set_resolved_conversation(conversation_id)
+                chatwoot.send_text(conversation_id, notify)
+                chatwoot.set_resolved_conversation(conversation_id)
+
 
 class ChatwootInstance(models.Model):
     _inherit = 'chatwoot.instance'
